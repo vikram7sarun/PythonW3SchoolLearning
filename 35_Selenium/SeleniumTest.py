dc22d680916f42bfd736c7f676a3ce646a5f1fbb
Vikram <45116392+vikram7sarun@users.noreply.github.com>
2022-09-28 00:39:58 +0530

    code update

--- 35_Selenium/SeleniumTest.py
+++ 35_Selenium/SeleniumTest.py
@@ -1,12 +1,22 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
-
-
 driver.maximize_window()
+driver.find_element(By.XPATH, "//input[@title='Search']").send_keys("Python")
+element.send_keys(" and some", Keys.ARROW_DOWN)
 
-driver.find_element(By.NAME,"").send_keys("")
+
+
+menu = driver.find_element_by_css_selector(".nav")
+hidden_submenu = driver.find_element_by_css_selector(".nav # submenu1")
+
+actions = ActionChains(driver)
+actions.move_to_element(menu)
+actions.click(hidden_submenu)
+actions.perform()
---
+++ ______Test______/1_ListOfCharacterToUpper.py
@@ -0,0 +1,4 @@
+names = ["alice", "bob", "charlie"]
+names_upper = [name.upper() for name in names]
+print(names_upper)
+
