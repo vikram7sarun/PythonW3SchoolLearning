94fa1c6732649e79211524d3fabe05c531fac888
vikic7 <p5200607@ness.com>
2024-10-03 00:04:20 -0400

    Selenium Test Update

--- 35_Selenium/SeleniumTest.py
+++ 35_Selenium/SeleniumTest.py
@@ -1,21 +1,18 @@
-import time
-
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 driver.get("https://www.google.com")
 driver.maximize_window()
 driver.find_element(By.XPATH, "//input[@title='Search']").send_keys("Python")
-element.send_keys(" and some", Keys.ARROW_DOWN)
-
-
-
-menu = driver.find_element_by_css_selector(".nav")
-hidden_submenu = driver.find_element_by_css_selector(".nav # submenu1")
-
+menu = driver.find_element(By.CSS_SELECTOR,".nav")
+hidden_submenu = driver.find_element(By.CSS_SELECTOR,".nav # submenu1")
 actions = ActionChains(driver)
 actions.move_to_element(menu)
 actions.click(hidden_submenu)
