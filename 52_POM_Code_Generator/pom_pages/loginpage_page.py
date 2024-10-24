fa88e3290721ee047506d28fcfc2abe0248bb738
vikic7 <p5200607@ness.com>
2024-10-23 20:35:41 -0400

    Remote Debugging using PlayWright and selenium

---
+++ 52_POM_Code_Generator/pom_pages/loginpage_page.py
@@ -0,0 +1,38 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage:
+    """Page Object for Loginpage page"""
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.username_field_locator = (By.ID, "username")
+        self.password_field_locator = (By.ID, "password")
+        self.login_button_locator = (By.XPATH, "//button[@type='submit'']")
+
+    def click_username_field(self):
+        """Click the username_field element"""
+        self.driver.find_element(*self.username_field_locator).click()
+
+    def get_username_field_text(self):
+        """Get the text of the username_field element"""
+        return self.driver.find_element(*self.username_field_locator).text
+
+    def click_password_field(self):
+        """Click the password_field element"""
+        self.driver.find_element(*self.password_field_locator).click()
+
+    def get_password_field_text(self):
+        """Get the text of the password_field element"""
+        return self.driver.find_element(*self.password_field_locator).text
+
+    def click_login_button(self):
+        """Click the login_button element"""
+        self.driver.find_element(*self.login_button_locator).click()
+
+    def get_login_button_text(self):
+        """Get the text of the login_button element"""
+        return self.driver.find_element(*self.login_button_locator).text
+
